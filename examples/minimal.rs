d5e2a2676fcebf22323844fd1d8be72e4d57de22
marceline-cramer <cramermarceline@gmail.com>
2024-03-05 21:50:20 -0700

    Zoom in minimal example

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -24,7 +24,7 @@ fn setup(mut commands: Commands, assets: ResMut<AssetServer>) {
 
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 0.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
