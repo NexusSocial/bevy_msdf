7c33d6e2ed9c3a50f787028a28e24fc5e9c30e9d
marceline-cramer <cramermarceline@gmail.com>
2024-06-15 19:48:39 -0600

    scene example

---
+++ examples/scene.rs
@@ -0,0 +1,61 @@
+use bevy::prelude::*;
+use bevy_msdf::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(MsdfPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: Res<AssetServer>,
+) {
+    // circular base
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::rgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // text
+    commands.spawn((
+        MsdfText {
+            atlas: assets.load("mononoki-Regular.ttf"),
+            content: "Hello, world!".to_string(),
+            color: Color::WHITE,
+        },
+        Transform {
+            translation: Vec3::new(0.0, 1.0, 0.0),
+            rotation: Quat::IDENTITY,
+            scale: Vec3::splat(0.7),
+        },
+        GlobalTransform::default(),
+    ));
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
